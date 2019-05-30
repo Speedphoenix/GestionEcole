cc34330867900f94ed09bec1d1efe67548ee5815
Speedphoenix <leonardo.jeanteur@gmail.com>
2019-05-30 19:57:30 +0200

    added getStartYear() and getEndYear() in class AnneeScolaire

--- src/com/speedphoenix/Modele/AnneeScolaire.java
+++ src/com/speedphoenix/Modele/AnneeScolaire.java
@@ -1,6 +1,8 @@
 package com.speedphoenix.Modele;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class AnneeScolaire extends BaseElem{
     protected ArrayList<Trimestre> trimestres;
@@ -12,12 +14,37 @@ public class AnneeScolaire extends BaseElem{
         Ecole.getInstance().addAnneeScolaire(this);
     }
 
-
     public void addTrimestre(Trimestre what) {
         trimestres.add(what);
     }
 
     public void addClasse(Classe what) {
         classes.add(what);
+    }
+
+    public int getStartYear() {
+        if (trimestres.isEmpty())
+            return 0;
+        Iterator<Trimestre> i = trimestres.iterator();
+        LocalDate earliest = i.next().getDebut();
+        while (i.hasNext()) {
+            LocalDate curr = i.next().getDebut();
+            if (curr.compareTo(earliest) < 0)
+                earliest = curr;
+        }
+        return earliest.getYear();
+    }
+
+    public int getEndYear() {
+        if (trimestres.isEmpty())
+            return 0;
+        Iterator<Trimestre> i = trimestres.iterator();
+        LocalDate latest = i.next().getFin();
+        while (i.hasNext()) {
+            LocalDate curr = i.next().getDebut();
+            if (curr.compareTo(latest)  > 0)
+                latest = curr;
+        }
+        return latest.getYear();
     }
 }
